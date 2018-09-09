2f893f5860b646649b064b6a337119b9dc15735f
benedictuttley <benedictuttley@hotmail.com>
2018-09-09 17:38:31 +0100

    Upload new additions, module improved.

--- annotation-uploader.js
+++ annotation-uploader.js
@@ -1,120 +1,127 @@
-//Module to read in annottaions associated with new image uploads.
+// Module to read in annotations associated with new image uploads.
 const xlstojson = require('xls-to-json');
 const mysql = require('mysql');
 const conn = require('./mysql_setup');
 const winston = require('./config/winston'); // Log various actions and store logs in /logs/app.log
 const async = require('async');
-module.exports.test = function(conn, filename, spreadsheetContent, callback){
-console.log("how?");
-xlstojson({
-      input: filename, // input xls
-      output: "output.json", // output json
-      lowerCaseHeaders: true
-    }, (err, jsonxls) => {
-      // Dependent on the type of stylesheet, use the correct sql query.
-      console.log("---", spreadsheetContent);
-      switch (spreadsheetContent)
-      {
-        case "image_annotations":
-          console.log("33333333333");
-          async.mapSeries(jsonxls, upload_mysql, function (err, results) {
-            console.log("?????????????");
-            var number_successful = 0;
-            var unsuccessful_entrys = []
-            results.forEach(result => {
-              if (result[0]) number_successful = number_successful+1;
-              else unsuccessful_entrys.push(result[1]);
-            });
-            callback(number_successful, unsuccessful_entrys);
+
+module.exports.test = function(conn, filename, spreadsheetContent, callback) {
+  xlstojson({
+    input: filename, // input xls
+    output: "output.json", // output json
+    lowerCaseHeaders: true
+  }, (err, jsonxls) => {
+
+    // Dependent on the type of stylesheet, use the correct sql query.
+    switch (spreadsheetContent) {
+      case "image_annotations":
+
+        async.mapSeries(jsonxls, upload_mysql, function(err, results) {
+
+          var number_successful = 0;
+          var unsuccessful_entrys = []
+          results.forEach(result => {
+            if (result[0]) number_successful = number_successful + 1;
+            else unsuccessful_entrys.push(result[1]);
           });
-          break;
+          callback(number_successful, unsuccessful_entrys);
+        });
+        break;
 
-        case "description":
-          async.map(jsonxls, upload_mysql_desc, function (err, results) {
-            var number_successful = 0;
-            var unsuccessful_entrys = []
+      case "description":
+        async.map(jsonxls, upload_mysql_desc, function(err, results) {
+          var number_successful = 0;
+          var unsuccessful_entrys = []
 
-            results.forEach(result => {
-              if (result[0]) number_successful = number_successful+1;
-              else unsuccessful_entrys.push(result[1]);
-            });
-
-            callback(number_successful, unsuccessful_entrys);
-
+          results.forEach(result => {
+            if (result[0]) number_successful = number_successful + 1;
+            else unsuccessful_entrys.push(result[1]);
           });
-          break;
-          default:
-        }
-      });
-    };
+
+          callback(number_successful, unsuccessful_entrys);
+
+        });
+        break;
+      default:
+    }
+  });
+};
 
 
-// Schema that new excel Demo uploads need to follow:
-// Image name for corresponding image -- [Probe/id/date]
-// File name --> Becomes the Date
-// Rest of columns remain.
-
-
-var upload_mysql = function (row, cb) {
-  console.log("firrred!");
+var upload_mysql = function(row, cb) {
   var values = extract_data_for_probe_sequences_array(row);
-  var query = "INSERT INTO Demo(file_name, link_to_file, slide_name, date," +
+  let query = "INSERT INTO Demo(file_name, link_to_file, slide_name, date," +
     "user, probe, probe_concentration, genotype_a, genotype_b, objective," +
     "optivar, cmount, stages_shown_in_picture, description_of_staining_pattern," +
     "comments, xcoordinate, ycoordinate) VALUES (" + values + ")";
   conn.query(query, (err, res) => {
-    console.log("pppp");
-    if(!err) cb(null, [true, row]);
-    else if(err.code == "ER_DUP_ENTRY"){
+    if (!err) cb(null, [true, row]);
+    else if (err.code == "ER_DUP_ENTRY") {
       winston.error("Duplicate Demo data entered.");
       cb(null, [false, row]);
-    }
-   else cb(err, [false, row]);
- });
+    } else cb(err, [false, row]);
+  });
 }
-//
-function extract_data_for_probe_sequences_array(row) {
 
-// Dynacmically create the image link using other columns or make permanent requirment:
-//var link = row['Date'] + row['probe'] + row['name of image'];
-
-let row_to_upload = [];
-row_to_upload.push("'" + row['date']+ "'", "'" +row['file name']+"'","'"+row['slide name'] + "'", "'" + row['date']+ "'","'"+ row['user']+"'",
-                   "'"+row['probe']+"'", "'" + row['probe concentration'] + "'", "'"+row['genotype a']+"'", "'"+row['genotype b']+"'",
-                   "'"+row['objective']+"'","'"+ row['optivar']+"'", "'"+row['c-mount']+"'", "'"+row['stages shown in picture']+"'",
-                   "'"+row['description of staining pattern']+"'", "'"+row['comments']+"'", "'"+row['x-coordinate']+"'",
-                   "'"+row['y-coordinate']+"'");
-                   return row_to_upload;
-                 }
-
-
-var upload_mysql_desc = function (row, callback) {
+var upload_mysql_desc = function(row, callback) {
   var values = extract_data_for_description(row);
   var query = "INSERT INTO Probe_Annotations(Gene,Probe_Set,Transcript_ID," +
-    "Target_Description,`wt(tin)`,`wt(mip40)(excised)`,`wt(white)`,aly5,comr," +
-    "tomb,nht,nxt1,`mip40(sh2)(bsc)`,`mip40(ey)`,`mip40(ey)(bg4)`,wucRNAi,wucRNAi_aly,`mip40(sh2)(comr)`,`mip40(ey)(aly)`,`aly1(27)`,`aly1(18)`,`aly1(18)(btr)`)" +
-    "VALUES (" + values + ")";
+  "Target_Description,`wt(tin)`,`wt(mip40)(excised)`,`wt(white)`,aly5,comr," +
+  "tomb,nht,nxt1,`mip40(sh2)(bsc)`,`mip40(ey)`,`mip40(ey)(bg4)`,wucRNAi," +
+  "wucRNAi_aly,`mip40(sh2)(comr)`,`mip40(ey)(aly)`,`aly1(27)`,`aly1(18)`," +
+  "`aly1(18)(btr)`)" + "VALUES (" + values + ")";
 
-    conn.query(query, (err, res) => {
-      if(!err) callback(null, [true, row]);
-      else if(err.code == "ER_DUP_ENTRY"){
-        winston.error("Duplicate Demo data entered.");
-        callback(null, [false, row]);
-      }
-     else callback(err, [false, row]);
-    });
+  conn.query(query, (err, res) => {
+    if (!err) callback(null, [true, row]);
+    else if (err.code == "ER_DUP_ENTRY") {
+      winston.error("Duplicate Demo data entered.");
+      callback(null, [false, row]);
+    } else callback(err, [false, row]);
+  });
+}
+
+function extract_data_for_probe_sequences_array(row) {
+
+  let row_to_upload = [];
+  row_to_upload.push(conn.escape(row['date']), conn.escape(row['file name']), conn.escape(row['slide name']),
+  conn.escape(row['date']), conn.escape(row['user']), conn.escape(row['probe']), conn.escape(row['probe concentration']),
+  conn.escape(row['genotype a']), conn.escape(row['genotype b']),
+  conn.escape(row['objective']), row['optivar'], row['c-mount'], row['stages shown in picture'],
+  conn.escape(row['description of staining pattern']), conn.escape(row['comments']), conn.escape(row['x-coordinate']),
+  conn.escape(row['y-coordinate']));
+
+  return row_to_upload;
+}
+
+function extract_data_for_description(row) {
+
+  let row_to_upload = [];
+  row_to_upload.push(conn.escape(row['Gene Symbol']),conn.escape(row['Probe Set ID']),conn.escape(row['Transcript ID(Array Design)']),
+  conn.escape(row['Target Description']),conn.escape(row['wt-tin']),conn.escape(row['wt-mip40-excised']),conn.escape(row['wt-white']),
+  conn.escape(row['aly5']),conn.escape(row['comr']),conn.escape(row['tomb']),conn.escape(row['nht']),conn.escape(row['Nxt1']),
+  conn.escape(row['mip40(sh2/bsc)']),conn.escape(row['mip40(ey)']),conn.escape(row['mip40(ey);bg4']),conn.escape(row['wucRNAi']),
+  conn.escape(row['wucRNAi;aly']),conn.escape(row['mip40(sh2), comr']),conn.escape(row['mip40(ey);aly']),conn.escape(row['aly1-27']),
+  conn.escape(row['aly1-18']),conn.escape(row['aly1-18-btr']));
+
+  return row_to_upload;
 }
 
 
-function extract_data_for_description(row){
+module.exports.quickAdditionImageUpload = function(values, cb) {
+  let query = "INSERT INTO Demo(file_name, link_to_file, slide_name, date," +
+    "user, probe, probe_concentration, genotype_a, genotype_b, objective," +
+    "optivar, cmount, stages_shown_in_picture, description_of_staining_pattern," +
+    "comments, xcoordinate, ycoordinate) VALUES (" + values + ")";
+  conn.query(query, (err, res) => {
+    if (!err) {
+      cb(null);
+    } else if (err.code == "ER_DUP_ENTRY") {
+      winston.error("Duplicate Demo data entered.");
+      cb(err);
+    } else {
+      winston.error(`${err.status || 500} - ${err.message}`);
+      cb(err);
+    }
+  });
 
-  let row_to_upload = [];
-  row_to_upload.push("'" + row['Gene Symbol']+ "'", "'" +row['Probe Set ID']+"'","'"+row['Transcript ID(Array Design)'] + "'",
-                     "'"+row['Target Description'] + "'","'"+row['wt-tin'] + "'",
-                     "'" + row['wt-mip40-excised']+ "'","'"+ row['wt-white']+"'","'"+row['aly5']+"'",
-                     "'" + row['comr'] + "'", "'"+row['tomb']+"'", "'"+row['nht']+"'", "'"+row['Nxt1']+"'",
-                     "'"+ row['mip40(sh2/bsc)']+"'", "'"+row['mip40(ey)']+"'", "'"+row['mip40(ey);bg4']+"'",
-                     "'"+row['wucRNAi']+"'", "'"+row['wucRNAi;aly']+"'", "'"+row['mip40(sh2), comr']+"'",
-                     "'"+row['mip40(ey);aly']+"'", "'"+row['aly1-27']+"'", "'"+row['aly1-18']+"'", "'"+row['aly1-18-btr']+"'");
-                     return row_to_upload;
-                   }
+}
